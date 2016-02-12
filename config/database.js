50959e14bdd27feed4f00e66fa31731b72c4e5aa
EC2 Default User <ec2-user@ip-172-31-17-95.ec2.internal>
2016-02-12 20:46:43 +0000

    first commit

---
+++ config/database.js
@@ -0,0 +1,8 @@
+// config/database.js
+module.exports = {
+
+	//'url' : 'your-settings-here' //looks like mongodb://<users>:<pass>@mongo.onmodulus.net:27017/Mikah40t
+	//'url' : 'mongodb://user-ec2:@localhost:27017'
+	'url' : 'mongodb://localhost:27017'
+
+};
